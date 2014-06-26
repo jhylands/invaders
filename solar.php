ce8834fa2e58926e09e37f05b51706024dcdb2b0
jhylands <james.space.ict@gmail.com>
2014-06-26 14:47:58 +0100

    Space station created. Solar system moved to its own file

--- solar.php
+++ solar.php
@@ -70,26 +70,26 @@
             35,             // Field of view
             800 / 600,      // Aspect ratio
             0.1,            // Near plane
-            10000000000           // Far plane
+            10000           // Far plane
         );
         
-		camera.position.set( 0, 0, -10 );//inishiation of camera
+		camera.position.set( -50, -20, 0 );//inishiation of camera
         camera.lookAt( scene.position );
 		var keyboard = new THREEx.KeyboardState();
 		renderer.setClearColorHex( 0x000000, 1 );
 		
 		//setup lighting conditions
 		var lightcolor =  0xFFFFFF
-		var light = new THREE.PointLight( lightcolor );
-        light.position.set( 0, 10, -10 );
+		var light = new THREE.AmbientLight( lightcolor );
+        light.position.set( 0, 10, 0 );
 		scene.add(light);
 		var light4 = new THREE.PointLight( lightcolor );
-        light4.position.set( 0, 0,0 );
-		scene.add(light4);
+        light4.position.set( 0, 0,-10 );
+		scene.add(light);
 	var imagePrefix = "images/nebula-";
 	var directions  = ["xpos", "xneg", "ypos", "yneg", "zpos", "zneg"];
 	var imageSuffix = ".png";
-	var skyGeometry = new THREE.CubeGeometry( 5000000000, 5000000000, 5000000000 );
+	var skyGeometry = new THREE.CubeGeometry( 500, 500, 500 );
 	var imageURLs = [];
 	for (var i = 0; i < 6; i++)
 		imageURLs.push( imagePrefix + directions[i] + imageSuffix );
@@ -105,63 +105,92 @@
 	} );
 	var skyBox = new THREE.Mesh( skyGeometry, skyMaterial );
 	scene.add( skyBox );
-	var spaceStation = new Object();
-	spaceStation.cylinder = new Array();
-	spaceStation.plane = new Array();
-	var planeTexture = new THREE.ImageUtils.loadTexture('images/panels.jpg');
-	var planeMaterial = new THREE.MeshPhongMaterial({map:planeTexture});
-	var planeGeometry = new THREE.PlaneGeometry(0.3,1);
-	var nighty = Math.PI/2;
-	var pannelData = [{x:-2.35,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:-2.,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:-1.65,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:-1.3,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:1.3,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:1.65,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:2,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:2.35,y:0,z:0.65,Rx:nighty,Ry:0,Rz:0},{x:-2.35,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:-2.,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:-1.65,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:-1.3,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:1.3,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:1.65,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:2,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:2.35,y:0,z:-0.65,Rx:nighty,Ry:0,Rz:0},{x:0.65,y:0.5,z:0.85,Rx:nighty,Ry:0,Rz:nighty},{x:0.65,y:0.5,z:1.35,Rx:nighty,Ry:0,Rz:nighty},{x:-0.65,y:0.5,z:0.85,Rx:nighty,Ry:0,Rz:nighty},{x:-0.65,y:0.5,z:1.35,Rx:nighty,Ry:0,Rz:nighty}];
-	for(i=0;i<pannelData.length;i++){
-		spaceStation.plane[i] = new THREE.Mesh(planeGeometry,planeMaterial);
-		spaceStation.plane[i].material.side = THREE.DoubleSide;
-		spaceStation.plane[i].rotation.x = pannelData[i].Rx
-		spaceStation.plane[i].rotation.y = pannelData[i].Ry
-		spaceStation.plane[i].rotation.z = pannelData[i].Rz
-		spaceStation.plane[i].position.x = pannelData[i].x
-		spaceStation.plane[i].position.y = pannelData[i].y
-		spaceStation.plane[i].position.z = pannelData[i].z
-		scene.add(spaceStation.plane[i]);
+	var planetNames = new Array('sun.jpg','planet1.png','venus.jpg','earth.jpg','mars.jpg','moon.jpg');
+	var planetPositions = new Object();
+	planetPositions.x = new Array(-100,10,30,60,80,65);
+	planetPositions.z = new Array(0,0,0,0,0,20);
+	var planetSizes = new Array(20,3,4,5,3,1);
+	var planet = new Array();
+	for(i=0;i<planetNames.length;i++){
+		planetGeometry = new THREE.SphereGeometry(planetSizes[i],32,32);
+	        var planetTexture = new THREE.ImageUtils.loadTexture('images/' + planetNames[i]);
+		var planetMaterial = new THREE.MeshPhongMaterial({map:planetTexture});
+		planet[i] = new THREE.Mesh(planetGeometry,planetMaterial);
+		planetMaterial.map.WrapS = THREE.RepeatWrapping;
+		planetMaterial.map.WrapT = THREE.RepeatWrapping;
+		planet[i].position.x = planetPositions.x[i];
+		planet[i].position.z = planetPositions.z[i];
+		planet[i].position.y=0;
+		scene.add(planet[i]);
 	}
-	var bodyData =[{x:-2,y:0,z:0,Rx:0,Ry:0,Rz:nighty},{x:-1,y:0,z:0,Rx:0,Ry:0,Rz:nighty},{x:0,y:0,z:0,Rx:0,Ry:0,Rz:nighty},{x:1,y:0,z:0,Rx:0,Ry:0,Rz:nighty},{x:2,y:0,z:0,Rx:0,Ry:0,Rz:nighty},{x:0,y:-0.5,z:0,Rx:0,Ry:0,Rz:0},{x:0,y:0,z:0,Rx:0,Ry:0,Rz:0},{x:0,y:0.5,z:0,Rx:0,Ry:0,Rz:0},{x:0,y:0.5,z:0,Rx:nighty,Ry:0,Rz:0},{x:0,y:0.5,z:1,Rx:nighty,Ry:0,Rz:0}];
-	var cylinderMaterial = new THREE.MeshPhongMaterial({color:0x000000,ambient:0xc0c0c0,specular:0xd0d0d0,shininess:1});
-	var cylinderGeometry = new THREE.CylinderGeometry(0.15,0.15,1);
-	for(i=0;i<bodyData.length;i++){
-		spaceStation.cylinder[i] = new THREE.Mesh(cylinderGeometry, cylinderMaterial);
-		spaceStation.cylinder[i].rotation.x = bodyData[i].Rx;
-		spaceStation.cylinder[i].rotation.y = bodyData[i].Ry;
-		spaceStation.cylinder[i].rotation.z = bodyData[i].Rz;
-		spaceStation.cylinder[i].position.x = bodyData[i].x;
-		spaceStation.cylinder[i].position.y = bodyData[i].y;
-		spaceStation.cylinder[i].position.z = bodyData[i].z;
-		scene.add(spaceStation.cylinder[i]);
-	}
-
+	/*
+	var planetTexture2 = new THREE.ImageUtils.loadTexture('images/venus.jpg');
+	var planetGeometry2 = new THREE.SphereGeometry(4,32,32);
+	var planetMaterial2 = new THREE.MeshPhongMaterial({map:planetTexture2});
+	var planet2 = new THREE.Mesh(planetGeometry2,planetMaterial2);
+	planetMaterial2.map.WrapS = THREE.RepeatWrapping;
+	planetMaterial2.map.WrapT = THREE.RepeatWrapping;
+	planet2.position.x=0;
+	planet2.position.y=0;
+	planet2.position.z=0;
+	scene.add(planet2);
+	var planetTexture3 = new THREE.ImageUtils.loadTexture('images/earth.jpg');
+	var planetGeometry3 = new THREE.SphereGeometry(4,32,32);
+	var planetMaterial3 = new THREE.MeshPhongMaterial({map:planetTexture3});
+	var planet3 = new THREE.Mesh(planetGeometry3,planetMaterial3);
+	planetMaterial3.map.WrapS = THREE.RepeatWrapping;
+	planetMaterial3.map.WrapT = THREE.RepeatWrapping;
+	planet3.position.x=10;
+	planet3.position.y=0;
+	planet3.position.z=0;
+	scene.add(planet3);
+	var planetTexture4 = new THREE.ImageUtils.loadTexture('images/planet1.png');
+	var planetGeometry4 = new THREE.SphereGeometry(4,32,32);
+	var planetMaterial4 = new THREE.MeshPhongMaterial({map:planetTexture4});
+	var planet4 = new THREE.Mesh(planetGeometry4,planetMaterial4);
+	planetMaterial4.map.WrapS = THREE.RepeatWrapping;
+	planetMaterial4.map.WrapT = THREE.RepeatWrapping;
+	planet4.position.x = 20;
+	planet4.position.y=0;
+	planet4.position.z=0;
+	scene.add(planet4);
+	var planetTexture5 = new THREE.ImageUtils.loadTexture('images/sun.jpg');
+	var planetGeometry5 = new THREE.SphereGeometry(4,32,32);
+	var planetMaterial5 = new THREE.MeshPhongMaterial({map:planetTexture5});
+	var planet5 = new THREE.Mesh(planetGeometry5,planetMaterial5);
+	planetMaterial5.map.WrapS = THREE.RepeatWrapping;
+	planetMaterial5.map.WrapT = THREE.RepeatWrapping;
+	planet5.position.x = -10;
+	planet5.position.y=0;
+	planet5.position.z=0;
+	scene.add(planet5);*/
 		//runLoader();
 		function update() {
 			//COUNT FPS
 			frameCount++;
+			//ROTATE PLANETS
+			for(i=0;i<planetNames.length;i++){
+				planet[i].rotation.y +=0.01/planetSizes[i];
+			}
 			//CAMERA MOVEMENT
 			if(keyboard.pressed("up")){
-				camera.position.y +=1;
+				Crotation+=0.01;
 			}else if(keyboard.pressed("down")){
-				camera.position.y -=1;
+				Crotation-=0.01;
 			}
 			if(keyboard.pressed("left")){
 				camera.position.x -=1;
 			}else if(keyboard.pressed("right")){
 				camera.position.x +=1;
 			}if(keyboard.pressed("q")){
-				camera.position.z -=1;
+				Crad -=0.1;
 			}else if(keyboard.pressed("e")){
-				camera.position.z +=1;
+				Crad +=0.1;
 			}
-			//camera.position.y=Crad* Math.sin(Crotation);
-			//camera.position.z=Crad* Math.cos(Crotation);
+			camera.position.y=Crad* Math.sin(Crotation);
+			camera.position.z=Crad* Math.cos(Crotation);
 			//get the camera to look at the spaceship
-			var a = new THREE.Vector3(0,0,0);
-			camera.lookAt(a);
+			camera.lookAt( planet[0].position);
 		}
 	
 		//Render Loop
