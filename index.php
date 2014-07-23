4921dbe1e2b2fb669c346588070a1275382adc28
jhylands <james.space.ict@gmail.com>
2014-07-23 11:01:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+use Facebook\FacebookRequest;
+use Facebook\GraphUser;
+use Facebook\FacebookRequestException;
+
+if($session) {
+
+  try {
+
+    $user_profile = (new FacebookRequest(
+      $session, 'GET', '/me'
+    ))->execute()->getGraphObject(GraphUser::className());
+
+    echo "Name: " . $user_profile->getName();
+
+  } catch(FacebookRequestException $e) {
+
+    echo "Exception occured, code: " . $e->getCode();
+    echo " with message: " . $e->getMessage();
+
+  }   
+
+}
+?>
