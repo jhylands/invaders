a40a16f75219fde07ac41aaf1e52a158122907b8
= <=>
2015-11-21 21:41:31 +0000

    Created ship class for handling communication with database and a resource class for passing resource types around.

--- scripts/shipInfo.php
+++ scripts/shipInfo.php
@@ -4,16 +4,19 @@ $QRY = "SELECT * FROM users,ships,shipTypes,locations,Markets WHERE users.FID=sh
 //echo $QRY . $_COOKIE['User'];
 $result = mysqli_query($con,$QRY . $_COOKIE['User']);
 while($row = mysqli_fetch_array($result)){
-	$ship = $row;
+	$ShipCode = $row['ShipCode'];
 }
 //echo $ship;
 class resource{
-    function _construct($connection){
+    function __construct($connection){
         //require a connection to the database to check values
         $this->con = $connection;
         $this->ID = null;
         $this->Name = null;
         $this->Code = null;
+    }
+    function __toString(){
+        return "ID:$this->ID; Code:$this->Code; Name:$this->Name;";
     }
     //WARNING No check for valididty of id,name or code on creation
     function resourceFromID($ID){
@@ -28,7 +31,7 @@ class resource{
     function resourceFromName($Name){
         $this->Name = $Name;
         $query = "SELECT * FROM resources WHERE Name='$Name'";
-        $result = mysqli_query($this->con,$QRY);
+        $result = mysqli_query($this->con,$query);
         while($row = mysqli_fetch_array($result)){
                 $this->ID = $row['ResourceID'];
                 $this->Code = $row['Code'];
@@ -37,10 +40,10 @@ class resource{
     function resourceFromCode($Code){
         $this->Code = $Code;
         $query = "SELECT * FROM resources WHERE Code='$Code'";
-        $result = mysqli_query($this->con,$QRY);
+        $result = mysqli_query($this->con,$query);
         while($row = mysqli_fetch_array($result)){
                 $this->ID = $row['ResourceID'];
-                $this->Code = $row['Code'];
+                $this->Name = $row['Name'];
         }
     }
     function getID(){
@@ -56,18 +59,21 @@ class resource{
 
 //WARNING sets need to be followed by an update
 class ship{
-    function _construct($connection,$ShipCode){
+    function __construct($connection,$ShipCode){
         //assert ShipCode is int 
         $this->con = $connection;
         $this->ShipCode = $ShipCode;
-        $this->_updateShip();
+        $this->update();
+    }
+    function __toString(){
+        return json_encode(array("ship" => $this->_ship, "hold" => $this->_hold));
     }
     function update(){
         $this->_updateShip();
         $this->_updateHold();
     }
     function _updateShip(){
-        $QRY = "SELECT * FROM users,ships,shipTypes,locations,Markets WHERE users.FID=ships.OwnerID AND locations.PlaceID=Markets.PlaceID AND locations.PlaceID=ships.Location AND ships.ShipType=shipTypes.ShipType AND users.FID=$this->ShipCode";
+        $QRY = "SELECT * FROM ships,shipTypes,locations,Markets WHERE locations.PlaceID=Markets.PlaceID AND locations.PlaceID=ships.Location AND ships.ShipType=shipTypes.ShipType AND ships.ShipCode=$this->ShipCode";
         $result = mysqli_query($this->con,$QRY);
         while($row = mysqli_fetch_array($result)){
                 $ship = $row;
@@ -78,7 +84,7 @@ class ship{
         $QRY = "SELECT * FROM ships, cargo WHERE ships.HoldCode=cargo.HoldCode AND ships.ShipCode=$this->ShipCode";
         $result = mysqli_query($this->con,$QRY);
         while($row = mysqli_fetch_array($result)){
-                $this->_hold[$row['ResourceID']] = $row['Ammount'];
+                $this->_hold[$row['ResourceID']] = $row['Amount'];
         }
     }
     
@@ -90,7 +96,7 @@ class ship{
         return mysqli_query($this->con,$query);
     }
     function getName(){
-        return $this->ship['Name'];
+        return $this->_ship['Name'];
     }
     //security floor
     function setName($name){
@@ -106,9 +112,14 @@ class ship{
         return mysqli_query($this->con,$query);
     }
     function changeResource($Resource,$Amount){
-        $Amount = $this->getResource($Resource->getID()) + $Amount;
-        return $this->setResource($Resouce->getID(), $Amount);
+        $Amount = $this->getResource($Resource) + $Amount;
+        return $this->setResource($Resource, $Amount);
     }
-    
+    function getLocationURL(){
+        return $this->_ship['PlanetURL'];
+    }
+    function getPlaceName(){
+        return $this->_ship['PlaceName'];
+    }
 }
 ?>
