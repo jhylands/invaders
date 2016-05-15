73d3438b0ed5b02d031c4160a88c55740f8381df
jhylands <james.space.ict@gmail.com>
2016-05-15 13:08:51 +0100

    Set up orbit.js class

--- pages/orbit.js
+++ pages/orbit.js
@@ -1,14 +1,21 @@
 //generate the class 
 function orbitPage(renderer,scene,camera){
+
+	
 	/*
 	CONSTRUCTOR CODE
+
+	get information from server about current planet, lightitng ect
 	*/
 
 	//function to create page from nothing
 	function create(){
 		//add any planets
+		//getS('planet')
 		//set up lighting
+		//setLights(scene)
 		//add spacestation
+		//use an objectloader as this is a larger object
 		//setup space station overlay
 	}
 	
@@ -17,6 +24,11 @@ function orbitPage(renderer,scene,camera){
 		if(keyState.pressed("up"){
 			//code to be run when this happens
 		}
+	}
+	
+	//function to update scene each frame
+	function update(){
+		//pass
 	}
 }
 
