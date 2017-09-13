b937f469fb9273bab8077cd2e4fa24f97d111ca7
James Hylands <james.space.ict@gmail.com>
2017-09-13 09:38:55 +0100

    added error handling to consoleLoader

--- python/consoleLoader.py
+++ python/consoleLoader.py
@@ -1,11 +1,25 @@
+#This script managed the including of console mods from the consolemod folder
+#It requires that the mod file has HELP: somehelp ; somewhere in the file for
+#the help to be included
+#COMMAND: somecommand; somewhere for the command to switch to the class
+#class className extends Handler{ for the class include
+#If the file is not to be included then it should have
+#NOMOD! somewhere in the file
+
 import re,os
 #get a list of files
 files = os.listdir(r'../consolemod')
-files = [ i for i in files if i!='Handler.php']
+#files = [ i for i in files if i!='Handler.php']
 
 #need to add an error checking function based on nonetype of m
-def add(m):
-    pass
+def add(search,so,eo,data,info):
+    m = re.search(search,data)
+    if type(m)==type(None):
+        if info!='MOD':
+            print('Error no ' + info)
+        return 'ERROR'
+    else:
+        return data[m.start()+so:m.end()+eo]
 
 #for each file add command and classname to data structure
 pairs = []
@@ -14,13 +28,17 @@ for file in files:
     f = open('../consolemod/' + file,'r')
     data = f.read()
     f.close()
-    m = re.search('[C][O][M][M][A][N][D].*[;]', data)
-    command = data[m.start()+8:m.end()-1]
-    m = re.search('[c][l][a][s][s]\s*.*[{]',data)
-    theClass = data[m.start()+6:m.end()-17]
-    m = re.search('[H][E][L][P].*[;]',data)
-    helper = data[m.start()+5:m.end()-1]
-    pairs.append( ('consolemod/' + file, command,theClass,helper) )
+    #check for NOMOD!
+    mod = add('NOMOD!',0,0,data,'MOD')=='ERROR'
+    if(mod):
+        command = add('[C][O][M][M][A][N][D].*[;]',8,-1,data,'COMMAND')
+        theClass = add('[c][l][a][s][s]\s*.*[{]',6,-17,data,'class')
+        helper = add('[H][E][L][P].*[;]',5,-1,data,'HELP')
+        pairs.append( ('consolemod/' + file, command,theClass,helper) )
+    else:
+        #send the nomod has beeen found to console
+        print('NOMOD!')
+            
 
 imports =''
 helps=''
@@ -39,7 +57,7 @@ with open('../consoleTempl.php','r') as f:
     code = code.replace('//IMPORTCLASS INPUT',imports)
     code = code.replace('//HELP-TEXT INPUT',helps)
     code = code.replace('//CASE INPUT',case)
-print(code)
+#print(code)
 with open('../console.php','w') as f:
     f.write(code)
         
