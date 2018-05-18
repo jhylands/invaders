2ebce7f9296970cde06630726a667d8283587db8
jhylands <jhh521@york.ac.uk>
2018-05-19 00:29:33 +0100

    updated permissions on I class

--- scripts/shipInfo.php
+++ scripts/shipInfo.php
@@ -1,11 +1,10 @@
 <?php
-$QRY = "SELECT * FROM OldUsers,ships,shipTypes,locations,markets WHERE OldUsers.FID=ships.OwnerID AND locations.PlaceID=markets.PlaceID AND locations.PlaceID=ships.Location AND ships.ShipType=shipTypes.ShipType AND users.FID=";
-//echo $QRY . $_COOKIE['User'];
-session_start();
+$QRY = "SELECT * FROM OldUsers,ships,shipTypes,locations,markets WHERE OldUsers.FID=ships.OwnerID AND locations.PlaceID=markets.PlaceID AND locations.PlaceID=ships.Location AND ships.ShipType=shipTypes.ShipType AND OldUsers.FID=";
 $FID = $user->data()->id;
 $result = mysqli_query($con,$QRY . $FID);
 if(!$result){
-    include 'createUser.php';
+    echo "<script>document.write('" . $QRY. "');</script>";
+    die();
 }
 while($row = mysqli_fetch_array($result)){
   $ShipCode = $row['ShipCode'];
